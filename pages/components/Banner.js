9b94a9e6dddec717f8cfed207d08aabad3d52de4
Irtiza Hafiz <irtizahafiz07@protonmail.com>
2024-04-04 01:40:50 -0400

    improve banner image aspect ratio

--- pages/components/Banner.js
+++ pages/components/Banner.js
@@ -15,16 +15,19 @@ import Image from "next/image";
 export default function Banner() {
   return (
     <Center padding={[4, 140]}>
-      <Stack spacing={[12, 48]} direction={["column", "row"]}>
-        <Flex direction={"column"}>
-          <Heading color="#75c682" fontSize={["4xl", "8xl"]}>
+      <Stack
+        spacing={[12, 48]}
+        direction={["column", "column", "column", "row"]}
+      >
+        <VStack direction={"column"} align="left" spacing={12}>
+          <Heading color="#75c682" fontSize={["4xl", "5xl", "7xl"]}>
             Hi! I am Irtiza.
           </Heading>
-          <Heading color="#efe073" fontSize={["xl", "4xl"]}>
+          <Heading color="#efe073" fontSize={["xl", "3xl", "4xl"]}>
             An Engineering Manager by day, and a Blogger and YouTuber by night.
           </Heading>
-        </Flex>
-        <Image src="/irtiza-avatar-ayesha.png" width={400} height={400}></Image>
+        </VStack>
+        <Image src="/irtiza-avatar-ayesha.png" width={600} height={400}></Image>
       </Stack>
     </Center>
   );
