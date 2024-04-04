7be0e10fb1891d147273bc234bb1b801df278aa7
Irtiza Hafiz <irtizahafiz07@protonmail.com>
2024-04-03 22:17:35 -0400

    update body font

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "../styles/styles.css";
 import "@fontsource/montserrat";
+import "@fontsource/inter";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import Head from "next/head";
 
 const theme = extendTheme({
   fonts: {
     heading: `'Montserrat', sans-serif`,
-    body: `'Montserrat', sans-serif`,
+    body: `'Inter', sans-serif`,
   },
 });
 
