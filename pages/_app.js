8c130c93094f0c93ed0bab18d2e4c6bd2431447e
Irtiza Hafiz <irtizahafiz07@protonmail.com>
2024-04-04 23:04:56 -0400

    revert to old way of loading script

--- pages/_app.js
+++ pages/_app.js
@@ -19,16 +19,11 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>Irtiza Hafiz</title>
         <link rel="icon" href="/irtiza-avatar-ayesha.png" />
-        <Script
-          src="https://example.com/script.js"
-          data-website-id="67058922-3cc9-48e1-843e-d29e11286402"
-        />
-        //TODO: Might need to revert, if above Script does not work
-        {/* <script
+        <script
           defer
-          src=""
-          
-        ></script> */}
+          src="https://umami.irtizahafiz.com/script.js"
+          data-website-id="67058922-3cc9-48e1-843e-d29e11286402"
+        ></script>
       </Head>
       <Component {...pageProps} />;
     </ChakraProvider>
