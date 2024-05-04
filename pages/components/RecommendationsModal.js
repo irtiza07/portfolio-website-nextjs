e20148661f6af107380e79b3acc8b4536dcf3cf3
Irtiza Hafiz <irtizahafiz07@protonmail.com>
2024-05-03 21:54:23 -0400

    separate blog and youtube content for recommended modal

--- pages/components/RecommendationsModal.js
+++ pages/components/RecommendationsModal.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 
 import {
@@ -14,6 +15,12 @@ import {
   Button,
   HStack,
   Grid,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  VStack,
 } from "@chakra-ui/react";
 
 export default function RecommendationsModal({
@@ -29,21 +36,58 @@ export default function RecommendationsModal({
           margin={[3]}
           borderRadius={20}
           boxShadow="0 0 50px rgba(255, 0, 0, 0.5), 0 0 50px rgba(255, 255, 0, 0.5), 0 0 50px rgba(0, 255, 0, 0.5), 0 0 50px rgba(0, 0, 255, 0.5)"
+          width={[500]}
         >
           <ModalHeader>Irtiza's Related Content</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Grid gap={[5, 8]}>
-              {recommendations?.map((rec, index) => (
-                <Link href={rec[1]} target="_blank" key={index}>
-                  <HStack>
-                    <Text color="black" fontSize="lg">
-                      {rec[0]} <ExternalLinkIcon color="#f57373" />
-                    </Text>
-                  </HStack>
-                </Link>
-              ))}
-            </Grid>
+            <Tabs>
+              <TabList>
+                <Tab>Blog</Tab>
+                <Tab>YouTube</Tab>
+              </TabList>
+
+              <TabPanels>
+                <TabPanel>
+                  <Grid gap={[5, 8]}>
+                    {recommendations
+                      ?.filter((rec) => rec[3] === 0)
+                      .map((rec, index) => (
+                        <Link href={rec[1]} target="_blank" key={index}>
+                          <HStack>
+                            <Text color="black" fontSize="lg">
+                              {rec[0]} <ExternalLinkIcon color="#f57373" />
+                            </Text>
+                          </HStack>
+                        </Link>
+                      ))}
+                  </Grid>
+                </TabPanel>
+                <TabPanel>
+                  <Grid gap={[5, 8]}>
+                    {recommendations
+                      ?.filter((rec) => rec[3] === 1)
+                      .map((rec, index) => (
+                        <Link href={rec[1]} target="_blank" key={index}>
+                          <VStack>
+                            <Image
+                              src={rec[4]}
+                              alt="Irtiza Hafiz's avatar"
+                              width={360}
+                              height={90}
+                            ></Image>
+                            <HStack>
+                              <Text color="black" fontSize="lg">
+                                {rec[0]} <ExternalLinkIcon color="#f57373" />
+                              </Text>
+                            </HStack>
+                          </VStack>
+                        </Link>
+                      ))}
+                  </Grid>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
           </ModalBody>
 
           <ModalFooter>
