a509758b83f66fdc9808375911d4766ce10a85e3
Irtiza Hafiz <irtizahafiz07@protonmail.com>
2024-05-28 00:20:30 -0400

    updated referral links

--- pages/components/CreditCardGrid.js
+++ pages/components/CreditCardGrid.js
@@ -14,7 +14,7 @@ export default function CreditCardGrid() {
         imageURL="/amex-gold.png"
         cardName="American Express Gold"
         cardDescription="Best travel card for foodies"
-        referralURL="https://americanexpress.com/en-us/referral/gold-card?ref=MDIRTHlsVP&XLINK=MYCP"
+        referralURL="https://www.americanexpress.com/en-us/credit-cards/referral/prospect/card/personal/gold-card?CORID=M~D~I~R~T~H~l~s~V~P-1716869724197-335029737&GENCODE=349992856309180&XLINK=MYCP&extlink=US-MGM-SPA_WEB_MYCA-copypaste-784-201329-K3QE%3A9941&ref=MDIRTHlsVP&v=2"
       />
       <CreditCardItem
         imageURL="/venture-x.png"
@@ -26,13 +26,13 @@ export default function CreditCardGrid() {
         imageURL="/blue-business-plus.png"
         cardName="Blue Business Plus"
         cardDescription="Great no annual fee AMEX catch-all travel card"
-        referralURL="https://americanexpress.com/en-us/referral/bluebusinessplus-credit-card?ref=MDIRTHTjpg&XLINK=MYCP"
+        referralURL="https://www.americanexpress.com/en-us/credit-cards/referral/prospect/card/bluebusinessplus-credit-card?ref=MDIRTHTjpg&XLINK=MYCP"
       />
       <CreditCardItem
         imageURL="/hilton-business.png"
         cardName="Hilton Honors Business AMEX"
         cardDescription="A great hotel card for free nights"
-        referralURL="https://americanexpress.com/en-us/referral/hilton-honors-american-express-business-credit-card?ref=MDIRTHKZNs&XLINK=MYCP"
+        referralURL="https://www.americanexpress.com/en-us/credit-cards/referral/prospect/card/business/hilton-honors-american-express-business-credit-card?CORID=M~D~I~R~T~H~K~Z~N~s-1716869983733-457393160&GENCODE=349993147096747&XLINK=MYCP&extlink=US-MGM-SPA_WEB_MYCA-copypaste-1131-201329-K58R%3A9985&ref=MDIRTHKZNs&v=2"
       />
       <CreditCardItem
         imageURL="/freedom_flex.png"
@@ -44,7 +44,7 @@ export default function CreditCardGrid() {
         imageURL="/marriott-business.png"
         cardName="Marriott Bonvoy Business AMEX"
         cardDescription="A great hotel card for free nights"
-        referralURL="https://americanexpress.com/en-us/referral/marriott-bonvoy-business-american-express-card?ref=MDIRTHxnGK&XLINK=MYCP"
+        referralURL="https://www.americanexpress.com/en-us/credit-cards/referral/prospect/card/business/marriott-bonvoy-business-american-express-card?CORID=M~D~I~R~T~H~x~n~G~K-1716870001159-506418675&GENCODE=349993210455784&XLINK=MYCP&extlink=US-MGM-SPA_WEB_MYCA-copypaste-474-201329-K73M%3A9991&ref=MDIRTHxnGK&v=2"
       />
 
       <CreditCardItem
