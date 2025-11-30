3bdb40084bd1ca1dd77b66061ae88fa84a96792a
Irtiza Hafiz <irtizahafiz07@gmail.com>
2025-11-30 00:04:04 -0500

    finalized blog post; removed pop content section

--- pages/components/BodyContainer.js
+++ pages/components/BodyContainer.js
@@ -1,18 +1,14 @@
 import React from "react";
-import { Grid } from "@chakra-ui/react";
+import { Grid, VStack } from "@chakra-ui/react";
 
-import PopularContentContainer from "./PopularContentContainer";
+// import PopularContentContainer from "./PopularContentContainer";
 import BlogContainer from "./BlogContainer";
 
 export default function BodyContainer({ posts, popularPosts }) {
   return (
-    <Grid
-      padding={4}
-      gap={12}
-      templateColumns={{ base: "repeat(1, 1fr)", md: "3fr 1fr" }}
-    >
+    <VStack padding={12}>
       <BlogContainer posts={posts} />
-      <PopularContentContainer popularPosts={popularPosts} />
-    </Grid>
+      {/* <PopularContentContainer popularPosts={popularPosts} /> */}
+    </VStack>
   );
 }
